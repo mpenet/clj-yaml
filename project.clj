2ff93294568fbcd012c75d8c95ee6f1990a7433c
Max Penet <max.penet@gmail.com>
2012-03-09 13:38:55 +0100

    inc version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mpenet/clj-yaml "0.3.1"
+(defproject org.clojars.mpenet/clj-yaml "0.3.2"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "http://github.com/lancepantz/clj-yaml"
   :dependencies
